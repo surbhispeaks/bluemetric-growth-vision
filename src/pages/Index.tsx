89049172bff991b2d5c04773078586c7fc0c1680
surbhispeaks <82967607+surbhispeaks@users.noreply.github.com>
2025-05-16 14:40:22 -0400

    Update Index.tsx
    Removed FAQ from home page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,6 @@ import Layout from "@/components/layout/Layout";
 import Hero from "@/components/home/Hero";
 import Differentiators from "@/components/home/Differentiators";
 import Benefits from "@/components/home/Benefits";
-import FAQ from "@/components/home/FAQ";
 import CallToAction from "@/components/home/CallToAction";
 
 const Index = () => {
